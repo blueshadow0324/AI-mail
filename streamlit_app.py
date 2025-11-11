e572669736d8560145b34d4f0b241c672ff70dab
blueshadow0324 <blueshadow0324@gmail.com>
2025-11-11 17:26:27 +0100

    x3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import os
 import re
 import html
 import streamlit as st
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
